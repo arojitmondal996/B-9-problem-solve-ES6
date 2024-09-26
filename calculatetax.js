8b842f2a1f2e156e56380d6ef99c29401f1037c4
arojitmondal996e <arojitmondal996@gmail.com>
2024-09-26 12:21:55 +0600

    Problem 2 solved

--- calculatetax.js
+++ calculatetax.js
@@ -1,8 +1,8 @@
 // Problem: 1
-function calculateTax(income, expense){
-if (income < 0 || expense < 0 || expense > income){
-    return 'Invalid input'
-}
+function calculateTax(income, expense) {
+    if (income < 0 || expense < 0 || expense > income) {
+        return 'Invalid input'
+    }
 
 
     let savings = income - expense
@@ -17,3 +17,25 @@ console.log(calculateTax(5000, 1500));
 console.log(calculateTax(7000, 7000));
 console.log(calculateTax(-5000, 1500));
 console.log(calculateTax(5000, -1500));
+
+// Problem: 2
+function sendNotification(email) {
+    // system 1
+    // if (email.split('').indexOf('@') === -1){
+    //     return 'Invalid Email'
+    // }
+    // system 2
+    if (email.split('') .includes('@') === false) {
+        return 'Invalid Email'
+    }
+
+    let emailArr = email.split('@')
+    let notification = `${emailArr[0]} sent you an email from ${emailArr[1]}`
+    return notification
+}
+
+// test problem 2
+console.log(sendNotification('zihad@gmail.com'))
+console.log(sendNotification('farhan34@yahoo.com'))
+console.log(sendNotification('fahim234.hotmail.com'))
+console.log(sendNotification('sadiaBicloud.com'))
