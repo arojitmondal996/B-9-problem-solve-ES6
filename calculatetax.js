25a2233e442f96549a9bbaeb6b46cd2a3bded200
arojitmondal996e <arojitmondal996@gmail.com>
2024-09-26 13:10:34 +0600

    problem 4 solved

--- calculatetax.js
+++ calculatetax.js
@@ -11,12 +11,12 @@ function calculateTax(income, expense) {
 }
 
 // test problem:1 
-console.log(calculateTax(10000, 3000));
-console.log(calculateTax(34000, 1753));
-console.log(calculateTax(5000, 1500));
-console.log(calculateTax(7000, 7000));
-console.log(calculateTax(-5000, 1500));
-console.log(calculateTax(5000, -1500));
+// console.log(calculateTax(10000, 3000));
+// console.log(calculateTax(34000, 1753));
+// console.log(calculateTax(5000, 1500));
+// console.log(calculateTax(7000, 7000));
+// console.log(calculateTax(-5000, 1500));
+// console.log(calculateTax(5000, -1500));
 
 // Problem: 2
 function sendNotification(email) {
@@ -25,7 +25,7 @@ function sendNotification(email) {
     //     return 'Invalid Email'
     // }
     // system 2
-    if (email.split('') .includes('@') === false) {
+    if (email.split('').includes('@') === false) {
         return 'Invalid Email'
     }
 
@@ -35,7 +35,69 @@ function sendNotification(email) {
 }
 
 // test problem 2
-console.log(sendNotification('zihad@gmail.com'))
-console.log(sendNotification('farhan34@yahoo.com'))
-console.log(sendNotification('fahim234.hotmail.com'))
-console.log(sendNotification('sadiaBicloud.com'))
+// console.log(sendNotification('zihad@gmail.com'))
+// console.log(sendNotification('farhan34@yahoo.com'))
+// console.log(sendNotification('fahim234.hotmail.com'))
+// console.log(sendNotification('sadiaBicloud.com'))
+
+// problem 3
+function checkDigitsInName(name) {
+    if (typeof name !== 'string') {
+        return 'Invalid Input'
+    }
+
+    let nameArr = name.split('')
+    for (let char of nameArr) {
+        if (!isNaN(char)) {
+            return true
+        }
+    }
+    return false
+}
+
+// console.log(checkDigitsInName('Raj123'))
+// console.log(checkDigitsInName('Suman'))
+// console.log(checkDigitsInName('Name2024'))
+// console.log(checkDigitsInName('!@#'))
+// console.log(checkDigitsInName(['Raj']))
+
+// problem 4
+function calculateFinalScore(obj) {
+    if (typeof obj != 'object' || Array.isArray(obj)) {
+        return 'Invalid Input'
+    }
+
+    let totalScore = obj.testScore + obj.schoolGrade
+    if (obj.isFFamily) {
+        totalScore += 20
+    }
+    return totalScore >= 80 ? true : false
+}
+
+console.log(
+    calculateFinalScore({
+        name: 'Rajib',
+        testScore: 45,
+        schoolGrade: 25,
+        isFFamily: true,
+    })
+)
+console.log(
+    calculateFinalScore({
+        name: 'Rajib',
+        testScore: 45,
+        schoolGrade: 25,
+        isFFamily: false,
+    })
+)
+console.log(
+    calculateFinalScore({
+        name: 'Rajib',
+        testScore: 50,
+        schoolGrade: 30,
+        isFFamily: false,
+    })
+)
+console.log(
+    calculateFinalScore("hello")
+)
